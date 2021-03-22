13fdc9906ebed2998c18eb909ac4f835f0242eb5
Lance Dang <lancedang64@gmail.com>
2021-03-22 16:54:16 +1100

    renamed storeNumber() to numberSelect(), added updateUI(), operate(), calculateTotal() functions. Now, firstNo and secondNo are strings, they are updated when numberButtons are used. When calculated, they are parsed into Integers and changed back to String afterwards.

--- myscript.js
+++ myscript.js
@@ -16,26 +16,77 @@ function divide(a,b) {
     return a / b;
 }
 
-function operate(e) {
-    
-}
-
-function storeNumber(e) {
+function numberSelect(e) {
     const numberSelected = e.target.className.slice(2);
-    updateNumber(numberSelected);
+    console.log(numberSelected);
+    if (operator == '') updateUI("updateFirstNo", numberSelected);
+    else updateUI("updateSecondNo", numberSelected);
 }
 
-function updateNumber(string) {
-    displayBar.textContent = displayBar.textContent.concat(string);
-    firstNo = firstNo.concat(string);
+function updateUI(func, string) {
+    switch (func) {
+        case "clear":
+            displayBar.textContent = '';
+            //un-highlight operator
+            break;
+        case "updateFirstNo":
+            firstNo = firstNo.concat(string);
+            displayBar.textContent = firstNo;
+            break;
+        case "updateSecondNo":
+            secondNo = secondNo.concat(string);
+            displayBar.textContent = secondNo;
+            break;    
+        case "selectOperator":
+            //highlight operator
+            break;
+        case "displayResult":
+            displayBar.textContent = result;
+            break;
+        default:
+            displayBar.textContent = "oops";
+    }
+}
+
+function operate(e) {
+    updateUI("selectOperator");
+    operator = e.target.className;
+}
+
+function calculateTotal() {
+    firstNo = parseInt(firstNo);
+    secondNo = parseInt(secondNo);
+
+    switch (operator) {
+        case "divide":
+            result = divide(firstNo,secondNo);
+            break;
+        case "multiply":
+            result = multiply(firstNo,secondNo);
+            break;
+        case "subtract":
+            result = subtract(firstNo,secondNo);
+            break;
+        case "add":
+            result = add(firstNo,secondNo);
+            break;
+        default:
+            console.log("oops calculateTotal() error");
+    }
+    updateUI('displayResult');
+    firstNo = firstNo.toString();
+    secondNo = secondNo.toString();
 }
 
 function clearCalculator(e) {
     displayBar.textContent = '';
     firstNo = '';
     secondNo = '';
+    operator = '';
+    result = 0;
 }
 
+let operator = '';
 let firstNo = '';
 let secondNo = '';
 let result = 0;
@@ -44,7 +95,7 @@ const displayBar = document.querySelector("div.display");
 
 const numberButtons = document.querySelectorAll("button#numberButton");
 numberButtons.forEach(numberButton => {
-    numberButton.addEventListener("click", storeNumber);
+    numberButton.addEventListener("click", numberSelect);
 });
 
 const operatorButtons = document.querySelectorAll("button#operatorButton");
@@ -55,3 +106,5 @@ operatorButtons.forEach(operatorButton => {
 const clearButton = document.querySelector("button.clear");
 clearButton.addEventListener("click", clearCalculator);
 
+const equalButton = document.querySelector("button.equal");
+equalButton.addEventListener("click", calculateTotal);
