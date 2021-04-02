d41de8ec14d7122b6cee7bee2cd6cdac5ff31bf4
Lance Dang <lancedang64@gmail.com>
2021-04-03 00:39:51 +1100

    Changed how result number is rounded
    The old way would return NaN if the number is too big
    New way shorter!

--- myscript.js
+++ myscript.js
@@ -95,7 +95,7 @@ function calculateResult() {
         default:
             console.log("oops calculateResult() error");
     }
-    updateDisplay(roundNumber(result,8));
+    updateDisplay(Number((result).toFixed(20)));
     
     firstNo = result;
     repeatNo = secondNo;
@@ -137,21 +137,6 @@ function changePositiveNegativeSign() {
         updateDisplay(secondNo);
     }
 }
-
-//got from StackOverflow
-function roundNumber(num, scale) {
-    if(!("" + num).includes("e")) {
-      return +(Math.round(num + "e+" + scale)  + "e-" + scale);
-    } else {
-      var arr = ("" + num).split("e");
-      var sig = ""
-      if(+arr[1] + scale > 0) {
-        sig = "+";
-      }
-      return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
-    }
-  }
-
 
 function clearCalculator(e) {
     displayBar.textContent = '';
